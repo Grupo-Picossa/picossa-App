8ceccf9fee71623f9a92918916a72251719e7960
Your Nameandres <aneduardotejada@gmail.com>
2020-06-24 21:28:25 -0500

    capturar campos de compras

--- assets/compras.js
+++ assets/compras.js
@@ -1,23 +1,54 @@
-const comprasBtn = document.getElementById('comprasBtn')
+const comprasBtn = document.getElementById('grabarBtn')
 
-
+const formulario = document.getElementById('formulariocompras')
 
 //eventlistener
 eventlistener()
 
 function eventlistener(){
 
+formulario.addEventListener('submit',capturarDatos)
+
 document.addEventListener('DOMContentLoaded',iniciarCompras)
 
 
 }
 
-console.log(comprasBtn)
+
 
 //funciones
 
 function iniciarCompras(){
-    
-    comprasBtn.disabled = true
+
     
 }
+
+function capturarDatos(e){
+    e.preventDefault()
+    
+    const ordenPedido = document.getElementById('ordenpedido').value
+    const costoEnvio = document.getElementById('costoEnvio').value
+    const trm= document.getElementById('trm').value
+    const fecha = document.getElementById('fecha').value
+    
+
+    const referencia = document.getElementById('referencia').value
+    const descripcion = document.getElementById('descripcion').value
+    const talla = document.getElementById('talla').value
+    const peso = document.getElementById('peso').value
+    const cantidad = document.getElementById('cantidad').value    
+    const precio = document.getElementById('precio').value
+    
+    
+    
+    
+    
+
+
+
+console.log(ordenPedido)
+
+
+
+}
+
